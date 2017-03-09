44eed1d6334621fb30198806cb28c4ccb2adc1b4
cloudray8580 <240078316@qq.com>
2017-03-09 23:56:33 +0800

    update

--- myserver/models.py
+++ myserver/models.py
@@ -5,4 +5,4 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.TextField(max_length=20000)
